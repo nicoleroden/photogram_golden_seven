bc8a22dfeeda4490e6cf70d56a253df37fdb4efb
nicoleroden <nicole.roden@gmail.com>
2015-05-06 15:03:50 -0500

    all to date

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,39 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @id = params[:id].to_i
+    @p=Photo.find_by({:id => @id})
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+     @p=Photo.new
+     @p.id =Photo.count+1
+     @p.source =[:the_source]
+     @p.caption = params[:the_caption]
+     @p.created_at=Date.today.to_s
+     @p.updated_at=Date.today.to_s
+     @p.save
+
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def destroy
+
+    @p=Photo.find_by({:id => params[:id]})
+    @p.destroy
+
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def edit_form
+      @p=Photo.find_by({:id => params[:id]})
+  end
 
 end
